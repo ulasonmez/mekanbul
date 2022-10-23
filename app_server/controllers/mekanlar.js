b3e9e538aa48141b10ca1ad51ebb716d17617fa4
ulasonmez <ulasonmez@outlook.com>
2022-10-23 18:27:18 +0300

    kod düzeltildi

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,20 +1,77 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-  const anaSayfa = function(req, res, next) {
-    res.render('anasayfa', { title: 'Ana Sayfa' });
-  }
-  
-  const mekanBilgisi = function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
-  }
+const anaSayfa = function (req, res) {
+  res.render("anasayfa", {
+    "baslik": "Ana Sayfa",
+    "sayfaBaslik": {
+      "siteAd": "MekanBul",
+      "slogan": "Civardaki Mekanları Keşfet!",
+    },
+    "mekanlar": [
+      {
+        "ad": "Starbucks",
+        "adres": "Centrum Garden",
+        "puan": "4",
+        "imkanlar": ["Dünya Kahveleri", "Yiyecek", "Hızlı Wifi Bağlantısı"],
+        "mesafe": "100m",
+      },
+      {
+        "ad": "Gloria Jeans",
+        "adres": "Sdü Doğu Kampüsü",
+        "puan": "3",
+        "imkanlar": ["Kahve", "Çay", "Pasta"],
+        "mesafe": "5km",
+      },
+    ],
+  });
+};
 
-  const yorumEkle = function(req, res, next) {
-    res.render('yorumekle', { title: 'Yorum Ekle' });
-  }
+const mekanBilgisi = function (req, res) {
+  res.render("mekanbilgisi", 
+  { 
+    "baslik": "Mekan Bilgisi",
+    "mekanBaslik": "Starbucks",
+    "mekanDetay": {
+        "ad": "Starbucks",
+        "adres": "Centrum Garden AVM",
+        "puan": "4",
+        "imkanlar": ["Dünya Kahveleri", "Kekler", "Pastalar"],
+        "koordinatlar": {
+            "enlem": "37.7",
+            "boylam": "30.5"
+        },
+        "saatler": [
+            {
+                "gunler": "Pazartesi-Cuma",
+                "acilis": "9:00-23:00",
+                "kapali": "false"
+            },
+            {
+                "gunler": "Cumartesi-Pazar",
+                "acilis": "10:00-22:00",
+                "kapali": "false"
+            }
+        ],
+        "yorumlar": [
+            {
+                "yorumYapan": "Ulaş Sönmez",
+                "puan": "5",
+                "tarih": "23 Ekim 2022",
+                "yorumMetni": "GOAT KAHVE MEKANI."
+            }
+        ]
+    }
 
-  module.exports={
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle
-  }
+ });
+};
+
+const yorumEkle = function (req, res, next) {
+  res.render("yorumekle", { title: "Yorum Ekle" });
+};
+
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+};
