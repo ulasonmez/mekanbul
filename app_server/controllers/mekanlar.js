78972430b8503419f65298e3c242ae7bfdb1174b
ulasonmez <ulasonmez@outlook.com>
2022-12-04 18:26:15 +0300

    odev7

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,70 +1,98 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios");
 
+var apiSecenekleri = {
+  sunucu: "https://mekanbul.ulasonmez.repl.co",
+  apiYolu: "/api/mekanlar/",
+};
+var mesafeyiFormatla = function(mesafe){
+  var yeniMesafe, birim;
+  if(mesafe>1){
+    yeniMesafe = parseFloat(mesafe).toFixed(1);
+    birim = " km";
+  }
+  else{
+    yeniMesafe = parseInt(mesafe*1000,10);
+    birim = " m";
+  }
+  return yeniMesafe + birim;
+}
+
+var anaSayfaOlustur = function(res,mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+    mesaj = "API HATASI: Birşeyler ters gitti.";
+    mekanListesi=[];
+  }
+  else{
+    if(!mekanListesi.length){
+      mesaj="Civarda herhangi bir mekan yok.";
+    }
+  }
+  res.render("anasayfa",{
+    baslik:"Anasayfa",
+    sayfaBaslik:{
+    siteAd:"Mekanbul",
+    slogan:"Mekanları Keşfet",
+  },
+  mekanlar:mekanListesi,
+  mesaj:mesaj,
+  });
+}
 const anaSayfa = function (req, res) {
-  res.render("anasayfa", {
-    "baslik": "Ana Sayfa",
-    "sayfaBaslik": {
-      "siteAd": "MekanBul",
-      "slogan": "Civardaki Mekanları Keşfet!",
+  axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu,{
+    params:{
+      enlem:req.query.enlem,
+      boylam:req.query.boylam,
     },
-    "mekanlar": [
-      {
-        "ad": "Starbucks",
-        "adres": "Centrum Garden",
-        "puan": "4",
-        "imkanlar": ["Dünya Kahveleri", "Yiyecek", "Hızlı Wifi Bağlantısı"],
-        "mesafe": "100m",
-      },
-      {
-        "ad": "Gloria Jeans",
-        "adres": "Sdü Doğu Kampüsü",
-        "puan": "3",
-        "imkanlar": ["Kahve", "Çay", "Pasta"],
-        "mesafe": "5km",
-      },
-    ],
+  }).then(function(response){
+    var i,mekanlar;
+    mekanlar = response.data;
+    for (i=0;i<mekanlar.length;i++){
+      mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
+    }
+    anaSayfaOlustur(res,mekanlar);
+  }).catch(function(hata){
+    anaSayfaOlustur(res,hata);
+  });
+};
+var detaySayfasiOlustur = function(res,mekanDetaylari){
+  mekanDetaylari.koordinat={
+    "enlem": mekanDetaylari.koordinat[0],
+    "boylam": mekanDetaylari.koordinat[1],
+  }
+  res.render('mekanbilgisi',
+  {
+    mekanBaslik: mekanDetaylari.ad,
+    mekanDetay: mekanDetaylari
+  });
+};
+var hataGoster = function(res,hata){
+  var mesaj;
+  if(hata.response.status=404){
+    mesaj= "404, Sayfa bulunamadı!";
+  }
+  else{
+    mesaj = hata.response.status + " hatası";
+  }
+  res.status(hata.response.status)
+  res.render('error', {
+    "mesaj":mesaj
   });
 };
 
 const mekanBilgisi = function (req, res) {
-  res.render("mekanbilgisi", 
-  { 
-    "baslik": "Mekan Bilgisi",
-    "mekanBaslik": "Starbucks",
-    "mekanDetay": {
-        "ad": "Starbucks",
-        "adres": "Centrum Garden AVM",
-        "puan": "4",
-        "imkanlar": ["Dünya Kahveleri", "Kekler", "Pastalar"],
-        "koordinat": {
-            "enlem": "37.7",
-            "boylam": "30.5"
-        },
-        "saatler": [
-            {
-                "gunler": "Pazartesi-Cuma",
-                "acilis": "9:00-23:00",
-                "kapali": "false"
-            },
-            {
-                "gunler": "Cumartesi-Pazar",
-                "acilis": "10:00-22:00",
-                "kapali": "false"
-            }
-        ],
-        "yorumlar": [
-            {
-                "yorumYapan": "İlayda Öztürk",
-                "puan": "4",
-                "tarih": "20 Ekim 2022",
-                "yorumMetni": "Kahveler iyi."
-            }
-        ]
-    }
-
- });
+  axios
+  .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+  .then(function(response){
+    detaySayfasiOlustur(res,response.data);
+  })
+  .catch(function(hata){
+    hataGoster(res,hata);
+  })
 };
+
 
 const yorumEkle = function (req, res, next) {
   res.render("yorumekle", { title: "Yorum Ekle" });
@@ -74,4 +102,6 @@ module.exports = {
   anaSayfa,
   mekanBilgisi,
   yorumEkle,
+  anaSayfaOlustur,
+  mesafeyiFormatla
 };
